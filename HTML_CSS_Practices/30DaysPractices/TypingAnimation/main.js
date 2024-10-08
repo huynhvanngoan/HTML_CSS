1b6489cf459e8cd4f3fff4ad9dd807ed009d9690
Huỳnh Văn Ngoãn <91590350+huynhvanngoan@users.noreply.github.com>
2024-10-08 22:57:44 +0700

    Typing animation

---
+++ HTML_CSS_Practices/30DaysPractices/TypingAnimation/main.js
@@ -0,0 +1,22 @@
+var p = document.querySelector('.typing-wrapper p')
+
+var originContent = p.innerHTML
+var index = 0
+var isForward = true
+setInterval(() => {
+    if(isForward) {
+        index++
+        if(index >= originContent.length) {
+            isForward = false
+        }
+    } else{
+        index--
+        if(index <= 0) {
+            isForward = true
+        }
+    }
+    p.innerHTML = originContent.substring(0, index)
+
+}, 200);
+
+
